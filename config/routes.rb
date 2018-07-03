60290f24c6747de32ccb9cb92a8d9a190cc7cff3
kazuki sumi <kazuki.sumi@techacademy.jp>
2018-07-03 14:50:53 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    #トップページのurl作成
+    root to: 'toppages#index'
 end
